1ca606ed7f5591d22ba90f3c3973118d3adfb837
Doug Jones <dsjr1284@gmail.com>
2020-03-29 00:58:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,13 +14,13 @@ class Song
 
   def self.new_by_name(name)
     song = Song.create
-    song.name = name 
+    song.name = name
     song
-  end 
-    
-    
-   
-  
+  end
+
+
+
+
 
 
   def save
